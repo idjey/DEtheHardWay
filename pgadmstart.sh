0acb66b3ee6d5c4f54c119609303e0101a075931
solverbot <kamaljp@gmail.com>
2022-11-03 07:13:34 +0530

    started the datamodeling

--- pgadmstart.sh
+++ pgadmstart.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 80:80 -e 'PGADMIN_DEFAULT_EMAIL=u@dom.l' -e 'PGADMIN_DEFAULT_PASSWORD=1579' --name dev-pgadmin -d dpage/pgadmin4
+docker run -p 80:80 -e 'PGADMIN_DEFAULT_EMAIL=u@dom.l' -e 'PGADMIN_DEFAULT_PASSWORD=1579' --name devgadmin -d dpage/pgadmin4
