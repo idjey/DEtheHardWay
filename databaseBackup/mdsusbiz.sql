df1e24a2a99e0042e6109efa7addfb224a572779
solverbot <kamaljp@gmail.com>
2022-11-05 09:54:12 +0530

    The mds data has been modeled and documented

--- databaseBackup/mdsusbiz.sql
+++ databaseBackup/mdsusbiz.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:536b9960d5f1b8a984c5fd2da685490bdd60b7633d5a5b336aed3721af9fc2a9
-size 3907092
+oid sha256:a61332fbafa5975a43d6c62f279855fe24a7af463f551be17feba7877ad4dc07
+size 4056418
